30fa74cecb2948071f9b400ca7021b692a5f781b
alexkramer <ackramer19@gmail.com>
2013-12-20 16:33:40 -0500

    Updating the license, scm, etc

--- ZipCityStateGrailsPlugin.groovy
+++ ZipCityStateGrailsPlugin.groovy
@@ -14,59 +14,27 @@ class ZipCityStateGrailsPlugin {
     // TODO Fill in these fields
     def title = "Zip City State Plugin" // Headline display name of the plugin
     def author = "Alex Kramer"
-    def authorEmail = ""
-    def description = '''\
-Provides utilities to populate the US City and State from the provided 5 digit zip code.
-'''
+    def authorEmail = "ackramer19@gmail.com"
+    def description = 'Provides utilities to populate the US City and State from the provided 5 digit zip code.'
 
     // URL to the plugin's documentation
-    def documentation = "github.com/alexkramer/ZipCityState"
+    def documentation = "https://github.com/alexkramer/ZipCityState"
 
     // Extra (optional) plugin metadata
 
     // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+    def license = "APACHE"
 
     // Details of company behind the plugin (if there is one)
     def organization = [ name: "Good Start Genetics", url: "http://www.goodstartgenetics.com/" ]
 
     // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+    def developers = [ [ name: "Alex Kramer", email: "ackramer19@gmail.com" ]]
 
     // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
+    def issueManagement = [ system: "GITHUB", url: "https://github.com/alexkramer/ZipCityState/issues" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
+    def scm = [ url: "https://github.com/alexkramer/ZipCityState" ]
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
 }
